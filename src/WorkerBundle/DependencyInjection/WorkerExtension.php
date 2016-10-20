32ef82b56d660d9e75bdc1dc8b955aac8b8dbc05
Thibaud Fabre <thibaud@aztech.io>
2016-10-20 12:53:37 +0200

    Allow bundle to work without loading queue bundle

--- src/WorkerBundle/DependencyInjection/WorkerExtension.php
+++ src/WorkerBundle/DependencyInjection/WorkerExtension.php
@@ -2,13 +2,24 @@
 
 namespace Alchemy\WorkerBundle\DependencyInjection;
 
+use Alchemy\Queue\MessageQueueRegistry;
+use Alchemy\QueueBundle\DependencyInjection\QueueConfiguration;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class WorkerExtension extends Extension
+class WorkerExtension extends ConfigurableExtension
 {
+
+    public function getConfiguration(array $config, ContainerBuilder $container)
+    {
+        return new QueueConfiguration('alchemy_worker');
+    }
+
     /**
      * Loads a specific configuration.
      *
@@ -19,14 +30,32 @@ class WorkerExtension extends Extension
      *
      * @api
      */
-    public function load(array $config, ContainerBuilder $container)
+    protected function loadInternal(array $config, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__.'/../Resources/config')
+            new FileLocator(__DIR__ . '/../Resources/config')
         );
 
         $loader->load('services.yml');
-    }
 
+        $registry = new Definition(MessageQueueRegistry::class);
+        $container->setDefinition('alchemy_queues.registry', $registry);
+
+        $queueConfigurations = $config['queues'];
+
+        if ($config['logger'] != '') {
+            $registry->addMethodCall('setLogger', [new Reference($config['logger'])]);
+        }
+
+        foreach ($queueConfigurations as $name => $configuration) {
+            $targetRegistry = $registry;
+
+            if (isset($configuration['registry'])) {
+                $targetRegistry = $container->getDefinition($configuration['registry']);
+            }
+
+            $targetRegistry->addMethodCall('bindConfiguration', [$name, $configuration]);
+        }
+    }
 }
